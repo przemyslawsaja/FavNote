1f65a34d1115884b4443c0fb6e03906a658924c2
przemyslawsaja <przemyslawsaja@gmail.com>
2020-04-08 19:33:40 +0200

    GlobalStyles added.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,26 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 const Button = styled.button`
-  background-color: #ffd82b;
+  padding: 0;
+  background-color: #E6E6E6;
+  width: ${({width}) => width || '220px'};
+  height: 47px;
+  border: none;
+  border-radius: 50px;
+  font-family: "Montserrat", sans-serif;
+  font-weight: 500;
+  font-size: 16px;
+  text-transform: uppercase;
+  
+  ${({secondary}) => (
+      secondary && css`
+        background-color: #FFD82B;
+        width:105px;
+        height: 30px;
+        font-size: 10px;
+      `
+)}
 `;
+
 
 export default Button;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Button from "../../components/Button/Button";
+import GlobalStyle from "../../theme/GlobalStyle";
 
-const Root = () =>
+const Root = () => (
     <div>
-        <h1> Hello from root !</h1>
-        <Button>Click me</Button>
-    </div>;
+        <GlobalStyle />
+        <h1>Hello Roman</h1>
+        <Button>Close / Save</Button>
+        <Button secondary>Remove</Button>
+    </div>
+);
+
 export default Root;
