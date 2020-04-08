bb44d72b345ef755afd67046d55b8206fb8e0454
przemyslawsaja <przemyslawsaja@gmail.com>
2020-04-08 16:37:56 +0200

    Base project structure, styled components package installed.

---
+++ src/views/Root/Root.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Button from "../../components/Button/Button";
+
+const Root = () =>
+    <div>
+        <h1> Hello from root !</h1>
+        <Button>Click me</Button>
+    </div>;
+export default Root;
