5d95009fb253962d58a5e47016025add9160b720
przemyslawsaja <przemyslawsaja@gmail.com>
2020-04-14 11:16:57 +0200

    rebuild

--- src/views/Root.js
+++ src/views/Root.js
@@ -1,30 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
-import MainTemplate from "../templates/MainTemplate"
-import Article from "./Article";
 import Notes from "./Notes";
+import store from "../store";
+import Article from "./Article";
 import Twitters from "./Twitters";
-import DetailsPage from "./DetailsPage";
 import { routes } from "../routes";
 import { Provider } from 'react-redux';
-import store from "../store";
+import DetailsPage from "./DetailsPage";
+import MainTemplate from "../templates/MainTemplate"
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
 
 const Root = () => (
-    <Provider store={store}>
-        <BrowserRouter>
-              <MainTemplate>
-                        <Switch>
-                            <Route exact path={routes.home} render={() => <Redirect to="notes"/>} />
-                            <Route exact path={routes.notes} component={Notes}/>
-                            <Route path={routes.note} component={DetailsPage}/>
-                            <Route exact path={routes.articles} component={Article}/>
-                            <Route path={routes.article} component={DetailsPage}/>
-                            <Route exact path={routes.twitters} component={Twitters}/>
-                            <Route path={routes.twitter} component={DetailsPage}/>
-                        </Switch>
-              </MainTemplate>
-        </BrowserRouter>
-    </Provider>
+        <Provider store={store}>
+
+            <BrowserRouter>
+                <MainTemplate>
+                    <Switch>
+                        <Route exact path={routes.home} render={() => <Redirect to="notes"/>} />
+                        <Route exact path={routes.notes} component={Notes}/>
+                        <Route path={routes.note} component={DetailsPage}/>
+                        <Route exact path={routes.articles} component={Article}/>
+                        <Route path={routes.article} component={DetailsPage}/>
+                        <Route exact path={routes.twitters} component={Twitters}/>
+                        <Route path={routes.twitter} component={DetailsPage}/>
+                    </Switch>
+                </MainTemplate>
+            </BrowserRouter>
+        </Provider>
 );
 
 export default Root;
