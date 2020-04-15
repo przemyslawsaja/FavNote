2538a0fc784d0d76dc9f733f693459c12703ded4
przemyslawsaja <przemyslawsaja@gmail.com>
2020-04-15 18:59:26 +0200

    heroku update

--- src/cms/cms.js
+++
@@ -1,6 +0,0 @@
-import CMS from "netlify-cms-app";
-import withStyled from "./with-styled";
-
-import MainTemplate from "../templates/MainTemplate";
-
-CMS.registerPreviewTemplate("main", withStyled(MainTemplate));
