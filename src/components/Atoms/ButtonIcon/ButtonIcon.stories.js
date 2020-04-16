1a5692833db13ce3d41f254224bfc2a742363b4c
przemyslawsaja <przemyslawsaja@gmail.com>
2020-04-16 19:14:06 +0200

    atoms added

---
+++ src/components/Atoms/ButtonIcon/ButtonIcon.stories.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import styled from 'styled-components';
+import ButtonIcon from './ButtonIcon';
+import { addDecorator } from '@storybook/react';
+import ThemeDecorator from "../../../../.storybook/preview";
+/*Icons*/
+import bulbIcon from '../../../assets/icons/bulb.svg'
+import logoutIcon from '../../../assets/icons/logout.svg'
+import penIcon from '../../../assets/icons/pen.svg';
+import plusIcon from '../../../assets/icons/plus.svg';
+import twitterIcon from '../../../assets/icons/twitter.svg';
+
+const YellowBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 500px;
+  height: 500px;
+  background-color: ${({ theme }) => theme.note};
+`;
+export default {
+    title: 'Atoms/ButtonIcon',
+    decorators: [storyFn => <YellowBackground>{storyFn()}</YellowBackground>],
+};
+
+export const Bulb = () => <ButtonIcon icon={bulbIcon}/>;
+export const Active = () => <ButtonIcon active icon={bulbIcon}/>;
+export const Logout = () => <ButtonIcon icon={logoutIcon}/>;
+export const Pen = () => <ButtonIcon icon={penIcon}/>;
+export const Plus = () => <ButtonIcon icon={plusIcon}/>;
+export const Twitter = () => <ButtonIcon icon={twitterIcon}/>;
